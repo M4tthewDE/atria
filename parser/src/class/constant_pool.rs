43fc829bd55b037f7a46fd5f37a213837a486f7c
M4tthewDE <matthias.kronberg@gmail.com>
2025-10-08 14:37:13 +0200

    feat: parse constant pool for java.lang.System

--- parser/src/class/constant_pool.rs
+++ parser/src/class/constant_pool.rs
@@ -1,9 +1,11 @@
 use std::io::Read;
 
 use anyhow::{Result, bail};
+use tracing::{debug, trace};
 
-use crate::util::{u1, u2, utf8};
+use crate::util::{u1, u2, u4, utf8};
 
+#[derive(Debug)]
 pub struct CpIndex(pub u16);
 
 impl From<u16> for CpIndex {
@@ -20,11 +22,18 @@ pub struct ConstantPool {
 
 impl ConstantPool {
     pub fn new(r: &mut impl Read, count: u16) -> Result<Self> {
+        // first item is reserved
+        let count = count - 1;
+
+        debug!("parsing {} constant pool items", count);
+
         let mut infos = Vec::with_capacity(count.into());
         infos.push(CpInfo::Reserved);
 
-        for _ in 0..count {
-            infos.push(CpInfo::new(r)?);
+        for i in 0..count {
+            let cp_info = CpInfo::new(r)?;
+            trace!("{}: {cp_info:?}", i + 1);
+            infos.push(cp_info);
         }
 
         Ok(Self { infos })
@@ -32,17 +41,22 @@ impl ConstantPool {
 }
 
 const UTF8_TAG: u8 = 1;
+const INTEGER_TAG: u8 = 3;
 const CLASS_TAG: u8 = 7;
 const STRING_TAG: u8 = 8;
 const FIELD_REF_TAG: u8 = 9;
 const METHOD_REF_TAG: u8 = 10;
 const INTERFACE_METHOD_REF_TAG: u8 = 11;
 const NAME_AND_TYPE_TAG: u8 = 12;
+const METHOD_HANDLE_TAG: u8 = 15;
+const METHOD_TYPE_TAG: u8 = 16;
 const INVOKE_DYNAMIC_TAG: u8 = 18;
 
+#[derive(Debug)]
 pub enum CpInfo {
     Reserved,
     Utf8(String),
+    Integer(u32),
     Class {
         name_index: CpIndex,
     },
@@ -65,6 +79,13 @@ pub enum CpInfo {
         name_index: CpIndex,
         descriptor_index: CpIndex,
     },
+    MethodHandle {
+        reference_kind: ReferenceKind,
+        reference_index: CpIndex,
+    },
+    MethodType {
+        descriptor_index: CpIndex,
+    },
     InvokeDynamic {
         bootstrap_method_attr_index: CpIndex,
         name_and_type_index: CpIndex,
@@ -77,12 +98,15 @@ impl CpInfo {
 
         match tag {
             UTF8_TAG => Self::utf8(r),
+            INTEGER_TAG => Self::integer(r),
             CLASS_TAG => Self::class(r),
             STRING_TAG => Self::string(r),
             FIELD_REF_TAG => Self::field_ref(r),
             METHOD_REF_TAG => Self::method_ref(r),
             INTERFACE_METHOD_REF_TAG => Self::interface_method_ref(r),
             NAME_AND_TYPE_TAG => Self::name_and_type(r),
+            METHOD_HANDLE_TAG => Self::method_handle(r),
+            METHOD_TYPE_TAG => Self::method_type(r),
             INVOKE_DYNAMIC_TAG => Self::invoke_dynamic(r),
             _ => bail!("invalid constant pool info tag {tag}"),
         }
@@ -91,6 +115,10 @@ impl CpInfo {
     fn utf8(r: &mut impl Read) -> Result<Self> {
         let length = u2(r)?;
         Ok(Self::Utf8(utf8(r, length.into())?))
+    }
+
+    fn integer(r: &mut impl Read) -> Result<Self> {
+        Ok(Self::Integer(u4(r)?))
     }
 
     fn class(r: &mut impl Read) -> Result<Self> {
@@ -133,10 +161,53 @@ impl CpInfo {
         })
     }
 
+    fn method_handle(r: &mut impl Read) -> Result<Self> {
+        Ok(Self::MethodHandle {
+            reference_kind: ReferenceKind::new(u1(r)?)?,
+            reference_index: u2(r)?.into(),
+        })
+    }
+
+    fn method_type(r: &mut impl Read) -> Result<Self> {
+        Ok(Self::MethodType {
+            descriptor_index: u2(r)?.into(),
+        })
+    }
+
     fn invoke_dynamic(r: &mut impl Read) -> Result<Self> {
         Ok(Self::InvokeDynamic {
             bootstrap_method_attr_index: u2(r)?.into(),
             name_and_type_index: u2(r)?.into(),
+        })
+    }
+}
+
+#[derive(Debug)]
+pub enum ReferenceKind {
+    GetField,
+    GetStatic,
+    PutField,
+    PutStatic,
+    InvokeVirtual,
+    InvokeStatic,
+    InvokeSpecial,
+    NewInvokeSpecial,
+    InvokeInterface,
+}
+
+impl ReferenceKind {
+    pub fn new(value: u8) -> Result<Self> {
+        Ok(match value {
+            1 => Self::GetField,
+            2 => Self::GetStatic,
+            3 => Self::PutField,
+            4 => Self::PutStatic,
+            5 => Self::InvokeVirtual,
+            6 => Self::InvokeStatic,
+            7 => Self::InvokeSpecial,
+            8 => Self::NewInvokeSpecial,
+            9 => Self::InvokeInterface,
+            _ => bail!("invalid reference kind {value}"),
         })
     }
 }
