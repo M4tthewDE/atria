feba6cb3d3c40f41ea207893af2bc2d61f95942c
M4tthewDE <matthias.kronberg@gmail.com>
2025-10-12 16:02:16 +0200

    feat: parse java.lang.Class <clinit> instructions

--- jvm/src/code.rs
+++ jvm/src/code.rs
@@ -12,6 +12,10 @@ impl Code {
         let mut i = 0;
         loop {
             match bytes[i] {
+                0x3 => {
+                    instructions.push(Instruction::Iconst(0));
+                    i += 1
+                }
                 0x12 => {
                     instructions.push(Instruction::Ldc(bytes[i + 1].into()));
                     i += 2
@@ -30,6 +34,16 @@ impl Code {
                     instructions.push(Instruction::InvokeVirtual(index.into()));
                     i += 3
                 }
+                0xb8 => {
+                    let index = (bytes[i + 1] as u16) << 8 | bytes[i + 2] as u16;
+                    instructions.push(Instruction::InvokeStatic(index.into()));
+                    i += 3
+                }
+                0xbd => {
+                    let index = (bytes[i + 1] as u16) << 8 | bytes[i + 2] as u16;
+                    instructions.push(Instruction::Anewarray(index.into()));
+                    i += 3
+                }
                 op_code => bail!("unknown instruction: 0x{op_code:x}"),
             }
 
@@ -42,8 +56,11 @@ impl Code {
 
 #[derive(Debug, Clone)]
 pub enum Instruction {
+    Iconst(u8),
     Ldc(CpIndex),
     Return,
     PutStatic(CpIndex),
     InvokeVirtual(CpIndex),
+    InvokeStatic(CpIndex),
+    Anewarray(CpIndex),
 }
